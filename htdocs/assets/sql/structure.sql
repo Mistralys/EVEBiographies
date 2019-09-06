08e5b3b64b08587df9a961ed8dd082cd833b998a
Mistralys <s.mordziol@gmail.com>
2019-09-06 20:49:42 +0200

    Updated with latest changes

--- htdocs/assets/sql/structure.sql
+++ htdocs/assets/sql/structure.sql
@@ -1,9 +1,9 @@
 ----
--- Table  characters
+-- Table structure for characters
 ----
 CREATE TABLE 'characters' ('character_id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'foreign_id' TEXT, 'name' TEXT, 'date_added' DATETIME, 'last_login' DATETIME, 'amount_logins' INTEGER DEFAULT 0, 'biography_id'  INTEGER NOT NULL  , 'slug' TEXT, 'portrait_filetype' TEXT DEFAULT 'jpg', 'terms_accepted' BOOLEAN, 'blocked' BOOLEAN DEFAULT 0);
 
 ----
--- Table  biographies
+-- Table structure for biographies
 ----
-CREATE TABLE 'biographies' ('biography_id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'text' TEXT, 'skin' TEXT, 'background' TEXT, 'font' TEXT, 'published' BOOLEAN DEFAULT 0, 'blocked' BOOLEAN DEFAULT 0);
+CREATE TABLE 'biographies' ('biography_id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'text' TEXT, 'skin' TEXT, 'background' TEXT, 'font' TEXT, 'published' BOOLEAN DEFAULT 0, 'blocked' BOOLEAN DEFAULT 0, 'words' INTEGER DEFAULT 0, 'views' INTEGER DEFAULT 0);
